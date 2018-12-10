dab0f88f38edfbc5da64c39150518316791486f3
Jsacharow <jsacharow@jennifers-mbp.attlocal.net>
2018-12-10 12:26:34 -0500

    created PEOPLE component to house individuial PERSON components

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -3,17 +3,19 @@ import Person from './Person/Person';
 
 
 
-const people  = (props) => 
-  props.people.map((person, index) => 
-     <Person
-      key={person.id}
-      changed = {(event) => props.changed(event, person.id )}
-      click={()=>props.clicked(index)}  // you can pass references to a method to a child component
-      name={person.name} 
-      age={person.age}>
-      {person.hobbies}
-    </Person>
-  
-  );
+const people  = (props) => {
+  return (
+    props.people.map((person, index) => 
+      <Person
+        key={person.id}
+        changed = {(event) => props.changed(event, person.id )}
+        click={()=>props.clicked(index)}  // you can pass references to a method to a child component
+        name={person.name} 
+        age={person.age}>
+        {person.hobbies}
+      </Person>
+    )
+  )
+};
 
 export default people;
