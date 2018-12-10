4e0898737987694d88ad9b6d45362acc12d43b99
Jsacharow <jsacharow@jennifers-mbp.attlocal.net>
2018-12-10 12:21:44 -0500

    created PEOPLE component to house individuial PERSON components

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import Person from './Person/Person';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 
 
-const people  = (props) => {
-  props.people.map((person, index)=> {
-    return <ErrorBoundary 
-    key = {person.id}  //<-- to manipulate the list more efficiently, also kills that annoying error
-    >
-    <Person
+
+const people  = (props) => 
+  props.people.map((person, index) => 
+     <Person
+      key={person.id}
       changed = {(event) => props.changed(event, person.id )}
       click={()=>props.clicked(index)}  // you can pass references to a method to a child component
       name={person.name} 
       age={person.age}>
       {person.hobbies}
     </Person>
-    </ErrorBoundary>
-  });
+  
+  );
 
-}
 export default people;
